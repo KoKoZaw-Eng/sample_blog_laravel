44df4ca9e6eebeee6ba11433e260e35213ede9ea
KoKoZaw <kokozawbeec@gmail.com>
2022-10-17 22:50:17 +0630

    starting blog

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header text-center">
+            Contents
+        </div>
+        <div class="card-body">
+            @foreach ($data as $post)
+                <div>
+                    <h5 class="card-title">{{ $post -> name }}</h5>
+                    <p class="card-text">{{ $post -> description }}</p>
+                    <a href="#" class="btn btn-primary">Read Deatail</a>
+                </div><br><hr>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+@endsection
